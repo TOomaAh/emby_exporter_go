9a80645fc533dcd2bf5f39641dea1d3b8b99b57d
TOomaAh <tbrunet72@gmail.com>
2026-09-07 14:33:57 +0200

    refacto: remove duplicate GetLibrarySize parameter

--- pkg/emby/server.go
+++ pkg/emby/server.go
@@ -196,7 +196,7 @@ func (s *Server) GetLibrarySize(itemID, contentType string) (int, error) {
 
 	req, err := s.requestManager.NewRequest(http.MethodGet, "/Users/"+
 		s.UserID+
-		"/Items?IncludeItemTypes=Movie&Recursive=true&Fields=BasicSyncInfo&EnableImageTypes=Primary&ParentId="+
+		"/Items?Recursive=true&Fields=BasicSyncInfo&EnableImageTypes=Primary&ParentId="+
 		itemID+"&Limit=1&IncludeItemTypes="+includeType[contentType], nil)
 
 	if err != nil {
